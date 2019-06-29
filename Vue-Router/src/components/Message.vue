0e7f35e323b45b1b7910c21fc2d721a84ff3136e
KaiKai <1035759181@qq.com>
2019-06-29 11:31:47 +0800

    修改目录以及README

--- Vue-Router/src/components/Message.vue
+++ Vue-Router/src/components/Message.vue
@@ -4,8 +4,11 @@
     <ul>
       <li v-for="(message) in messages" :key="message.id">
         <router-link :to="`/home/message/detail/${message.id}`">{{message.title}}</router-link>
+        <button @click="pushShow(message.id)">Push查看</button>
+        <button @click="replaceShow(message.id)">Replace查看</button>
       </li>
     </ul>
+    <button @click="$router.back()">回退</button>
     <router-view></router-view>
   </h3>
 </template>
@@ -36,7 +39,17 @@ export default {
         }]
         this.messages = messages
     }, 1000)
+  },
+
+  methods: {
+    pushShow(id) {
+      this.$router.push(`/home/message/detail/${id}`)
+    },
+    replaceShow(id){
+      this.$router.replace(`/home/message/detail/${id}`)
+    }
   }
+
 }
 
 </script>
